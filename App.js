5477af8f8635143921d9bbbf81ead92d6eb37796
Marcin Lesiak <marcinui07@gmail.com>
2025-04-28 12:56:43 +0200

    added a form to get data from the user

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [fuelBurnt, setFuelBurnt] = useState('');
+  const [distance, setDistance] = useState('');
+  const [fuelPer100, setFuelPer100] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Spalanie wyniosło {fuelPer100} litrów na 100 kilometrów.</Text>
+      <TextInput placeholder='Ilość spalonego paliwa' value={fuelBurnt} onChangeText={setFuelBurnt} keyboardType='numeric' />
+      <TextInput placeholder='Ilość przejechanych kilometrów' value={distance} onChangeText={setDistance} keyboardType='numeric' />
+      <TouchableOpacity>
+        <Text>Oblicz</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
